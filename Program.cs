9773ac644b2482300804f3847d2c4ff9c92f969d
chenaoh <jfarova112333@gmail.com>
2022-11-20 16:51:53 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int n1,n2;
+       Console.WriteLine("digite el primer numero");
+       n1=int.Parse(Console.ReadLine());
+       Console.WriteLine("digite el primer numero");
+       n2=int.Parse(Console.ReadLine());
+       if(n1>n2){
+        Console.WriteLine("el numero mayor es" + n1);
+
+
+       }
+       else{
+        Console.WriteLine("el numero mayor es" + n2);
+       }
+    }
+}
